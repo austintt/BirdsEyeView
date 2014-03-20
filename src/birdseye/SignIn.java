de85440a0f10802cb63cae5b81a8d2b098af1edd
Austin Tooley <tooley7@gmail.com>
2014-03-20 11:11:39 -0600

    Sign-In with Twitter works!!!

--- src/birdseye/SignIn.java
+++ src/birdseye/SignIn.java
@@ -13,45 +13,23 @@ import java.io.IOException;
 import twitter4j.auth.AccessToken;
 
 public class SignIn extends HttpServlet {
-    private static final long serialVersionUID = -6205814293093350242L;
+	private static final long serialVersionUID = -6205814293093350242L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Twitter twitter = new TwitterFactory().getInstance();
-
         request.getSession().setAttribute("twitter", twitter);
         try {
-            //Passing the callback url
             StringBuffer callbackURL = request.getRequestURL();
             int index = callbackURL.lastIndexOf("/");
-            callbackURL.replace(index, callbackURL.length(), "").append("/callback");
+            callbackURL.replace(index, callbackURL.length(), "").append("/Callback");
 
             RequestToken requestToken = twitter.getOAuthRequestToken(callbackURL.toString());
             request.getSession().setAttribute("requestToken", requestToken);
             response.sendRedirect(requestToken.getAuthenticationURL());
 
-
-            //Gets the OAuth access token added to the callback
-            requestToken = (RequestToken) request.getSession().getAttribute("requestToken");
-            String verifier = request.getParameter("oauth_verifier");
-            try {
-                twitter.getOAuthAccessToken(requestToken, verifier);
-                request.getSession().removeAttribute("requestToken");
-            } catch (TwitterException e) {
-                throw new ServletException(e);
-            }
-            response.sendRedirect(request.getContextPath() + "/");
-
-            } catch (TwitterException e) {
-                throw new ServletException(e);
-            }
+        } catch (TwitterException e) {
+            throw new ServletException(e);
+        }
 
     }
-
-    //TODO: This will access our SQL database to grab tokens for users that have already logged on.
-    private static AccessToken loadAccessToken(int useId){
-        //SELECT token, tokenSecret FROM user WHERE userId = useId
-        String token = "";// user.token
-        String tokenSecret = "";// user.tokenSecret
-        return new AccessToken(token, tokenSecret);
-  }
 }
